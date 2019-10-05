655d7d8b9e59d4c12738fac57738b6c0b878e437
danjha <ndanjha@icloud.com>
2019-10-05 18:14:40 -0400

    new basic sign in skeleton with shards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, FormInput, FormGroup } from "shards-react";
 
-function App() {
+export default function FormExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form>
+      <FormGroup>
+        <label htmlFor="#username">Username</label>
+        <FormInput id="#username" placeholder="Username" />
+      </FormGroup>
+      <FormGroup>
+        <label htmlFor="#password">Password</label>
+        <FormInput type="password" id="#password" placeholder="Password" />
+      </FormGroup>
+    </Form>
   );
 }
-
-export default App;
